5ba3d60e1be46a656b465f764dc68f596c75b8f3
Derek Yeh <djyeh@uci.edu>
2021-11-15 20:32:44 -0500

    add tests

--- test_api.py
+++ test_api.py
@@ -3,6 +3,7 @@ import requests
 import json
 import nltk.data
 import nltk
+from pathlib import Path
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
@@ -12,21 +13,26 @@ def call_classifier(info):
     return response.json()
 
 
-def process_text(filepath, labels):
+def process_text(filepath, labels, model):
     print("Loading file...")
-    with open(filepath) as f:
+    with open("test/"+filepath, encoding='utf-8') as f:
         info = json.load(f)
+    output = open("results/{}/{}-{}-result.txt".format(model, model, Path(filepath).stem), "w+", encoding='utf-8')
+
     print("Classifying text...")
     for line in tokenizer.tokenize(info["text"]):
         print(line)
         info["text"] = line
         results = call_classifier(info)
         print(results)
+        output.write(line + "\n")
+        output.write(str(results) + "\n")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--file_dir", default="input.json")
     parser.add_argument("--labels", default="anger,excitement,happiness,racism")
+    parser.add_argument("--model", default="deberta-v2-xxlarge")
     args = parser.parse_args()
-    process_text(args.file_dir, args.labels)
+    process_text(args.file_dir, args.labels, args.model)
