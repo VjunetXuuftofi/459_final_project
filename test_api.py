c1c6af0f8559f1684097f95bf9821287e1eaf5be
Derek Yeh <djyeh@uci.edu>
2021-11-18 17:41:03 -0500

    add combined roc and auc

--- test_api.py
+++ test_api.py
@@ -4,9 +4,12 @@ import json
 import nltk.data
 import nltk
 import numpy as np
+from sklearn import metrics
+
 from pathlib import Path
 import matplotlib.pyplot as plt
 import math
+import os
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
@@ -25,11 +28,33 @@ def process_text(filepath, labels, model):
     output = open("results/{}/".format(model) + result_file + "-result.txt", "w+", encoding='utf-8')
     probs = get_probs(output, info)
 
-    fpr, tpr, thresholds = calc_rates(probs, info)
+    target = []
+    for arr in info["gt"]:
+        target += arr
+    fpr, tpr, thresholds = calc_rates(probs, target)
 
     index = get_best_threshold_index(fpr, tpr)
 
     plot_roc_curve(fpr, tpr, model, result_file, index, thresholds[index])
+
+
+def test_all(model):
+    probs = []
+    target = []
+
+    for filename in os.listdir("test/amazon-reviews"):
+        with open("test/amazon-reviews/" + filename, encoding='utf-8') as f:
+            info = json.load(f)
+
+        for arr in info["gt"]:
+            target += arr
+
+        probs += get_probs(None, info)
+    
+    fpr, tpr, thresholds = calc_rates(probs, target)
+
+    index = get_best_threshold_index(fpr, tpr)
+    plot_roc_curve(fpr, tpr, model, "Amazon", index, thresholds[index])
 
 
 def get_best_threshold_index(fpr, tpr):
@@ -50,19 +75,18 @@ def get_probs(output, info):
         info["text"] = line
         results = call_classifier(info)
         print(results)
-        output.write(line + "\n")
-        output.write(str(results) + "\n")
+        if output:
+            output.write(line + "\n")
+            output.write(str(results) + "\n")
         probs += list(results.values())
     return probs
 
 
-def calc_rates(probs, info):
+def calc_rates(probs, target):
     thresholds = [x/100.0 for x in range(0,100,2)]
     fpr = []
     tpr = []
-    target = []
-    for arr in info["gt"]:
-        target += arr
+    
     target = np.array(target)
     for t in thresholds:
         prediction = np.zeros(len(probs))
@@ -84,7 +108,8 @@ def plot_roc_curve(fpr, tpr, model, result_file, index, best_threshold):
     plt.ylabel('True positive rate')
     plt.xlim([0,1])
     plt.ylim([0,1])
-    plt.title('ROC {} t={}'.format(result_file, best_threshold))
+    auc = metrics.auc(fpr, tpr)
+    plt.title('ROC {} t={} AUC={}'.format(result_file, best_threshold, auc))
     plt.savefig("results/{}/roc_plots/{}.png".format(model, result_file))
 
 
@@ -93,5 +118,9 @@ if __name__ == "__main__":
     parser.add_argument("--file_dir", default="input.json")
     parser.add_argument("--labels", default="anger,excitement,happiness,racism")
     parser.add_argument("--model", default="deberta-v2-xxlarge")
+    parser.add_argument("--test_all", default="False")
     args = parser.parse_args()
-    process_text(args.file_dir, args.labels, args.model)
+    if args.test_all == "True":
+        test_all(args.model)
+    else:
+        process_text(args.file_dir, args.labels, args.model)
